018e2b452f0c5b2b572fe318bbba5af22c74ba75
Kolhtics <baptiste-cervantes@homtail.fr>
2012-03-19 22:07:51 +0100

    ajout de keyboard.java

---
+++ src/Keyboard.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+	public class Keyboard {
+	private static BufferedReader keyboard=new BufferedReader(new InputStreamReader(System.in));
+	public static char getChar(){
+		return getString().charAt(0);
+		}
+
+	public static char[] getChars(){
+		String s = getString();
+		return s.toCharArray();
+		}
+
+	public static String getString() {
+		return getLine();
+		}
+
+	public static int getInt() {
+		try{
+			return Integer.parseInt(getLine());
+		}catch(NumberFormatException e){ return -1;}
+		}
+
+	public static void pause()
+		{
+		try{
+			keyboard.read();
+		}catch(IOException e){}
+		}
+
+	private static String getLine()
+		{
+		String line;
+		try{
+			line=keyboard.readLine();
+		}catch(IOException e){ return "";}
+		return line;
+		}
+}
