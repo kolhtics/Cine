8830fb544c038ff3edfbd8ab10433164a7437bb0
Kolhtics <baptiste-cervantes@homtail.fr>
2012-03-20 02:06:07 +0100

    ecouteur pour fermer la fenetre

---
+++ src/FermerFenetreEcouteur.java
@@ -0,0 +1,17 @@
+
+
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class FermerFenetreEcouteur extends WindowAdapter {
+	private Frame f;
+	
+	public FermerFenetreEcouteur(Frame f){
+		this.f=f;
+	}
+	
+	public void windowClosing(WindowEvent e){
+		f.dispose();
+	}
+}
