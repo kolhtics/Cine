9b8758531cc4f4fc15cb530723953f3c16de38c5
Kolhtics <baptiste-cervantes@homtail.fr>
2012-03-26 20:08:27 +0200

    panel d'affichage d'image pour frame1

---
+++ src/CinemaPackage/ImagePanel.java
@@ -0,0 +1,25 @@
+package CinemaPackage;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Observer;
+
+import javax.imageio.ImageIO;
+
+public class ImagePanel extends Panel{
+	
+	public ImagePanel(){
+	}
+	
+	public void paint(Graphics g){
+		try {
+            Image img = ImageIO.read(new File("Cinema.jpg"));
+            g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+		} catch (IOException e) {
+            
+		}
+
+	}
+
+}
