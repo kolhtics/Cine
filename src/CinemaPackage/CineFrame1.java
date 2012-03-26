f2f04cfcd475ead77b664d6bc57768654e632ed6
Kolhtics <baptiste-cervantes@homtail.fr>
2012-03-26 20:07:22 +0200

    ajout d'une fenetre 1 menu principal

---
+++ src/CinemaPackage/CineFrame1.java
@@ -0,0 +1,38 @@
+package CinemaPackage;
+
+import java.awt.*;
+
+/* changer la disposition des boutons */
+public class CineFrame1 extends Frame{
+	final static int HAUTEUR = 600;
+	final static int LARGEUR = 564;
+	
+	public CineFrame1(){
+		this.setTitle("Cinema -> Menu");
+		this.setSize(LARGEUR, HAUTEUR);
+		this.setLayout(new BorderLayout());
+		this.setBackground(Color.LIGHT_GRAY);
+		
+		Panel panelPrincipal = new Panel(new GridLayout(1,2));
+		
+		ImagePanel panelImage = new ImagePanel();
+		
+		Panel panelBouton = new Panel(new GridLayout(2,1));
+		Button bot= new Button("Mode Bot");
+		Button jeu =new Button("Mode Jeu");
+		
+		panelBouton.add(bot);
+		panelBouton.add(jeu);
+		
+		panelPrincipal.add(panelImage);
+		panelPrincipal.add(panelBouton);
+		
+		this.add(panelPrincipal, BorderLayout.CENTER);
+		
+		/* ecouteur */
+		this.addWindowListener(new FermerFenetreEcouteur(this));
+		this.setVisible(true);
+		
+		
+	}
+}
