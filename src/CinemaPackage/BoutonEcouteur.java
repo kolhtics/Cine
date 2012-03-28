817905a592af4fa1d5beccd4e7e5e2b8df54321d
Kolhtics <baptiste-cervantes@homtail.fr>
2012-03-28 14:54:02 +0200

    modification et ajout de quelques ecouteurs

--- src/CinemaPackage/BoutonEcouteur.java
+++ src/CinemaPackage/BoutonEcouteur.java
@@ -1,7 +1,6 @@
 package CinemaPackage;
 
-import java.awt.Button;
-import java.awt.Panel;
+import java.awt.Frame;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,25 +10,42 @@ public class BoutonEcouteur implements ActionListener {
 	private TextField t2;
 	private Repertoire<Acteur> lesActeurs;
 	private Repertoire<Film> lesFilms;
-	private Button b;
-	private Panel p;
+	private Frame f;
 	
-	public BoutonEcouteur(Button b, Panel p){
-		this.b=b;
-		this.p=p;
-	}
-	
-	public BoutonEcouteur(Button b, TextField t1, TextField t2, Repertoire<Acteur> lesActeurs, Repertoire<Film> lesFilms){
-		this.b=b;
-		this.t1=t1;
-		this.t2=t2;
+	public BoutonEcouteur(Frame f, Repertoire<Acteur> lesActeurs, Repertoire<Film> lesFilms){
+		this.f=f;
 		this.lesActeurs=lesActeurs;
 		this.lesFilms=lesFilms;
 	}
 	
+	public BoutonEcouteur(TextField t1, TextField t2, Repertoire<Acteur> lesActeurs, Repertoire<Film> lesFilms){
+		this(null, lesActeurs, lesFilms);
+		this.t1=t1;
+		this.t2=t2;
+	}
 	
+
 	public void actionPerformed(ActionEvent e) {
-		if(b.getLabel().equals("JOUER")) Test.testGraphique(t1.getText(), t2.getText(), lesActeurs, lesFilms);	
+		String bouton = e.getActionCommand();		
+		if(bouton.equals("Mode Jeu")) { f.dispose(); new CineFrame3(lesActeurs, lesFilms);}
+		if(bouton.equals("Mode Bot")) { f.dispose(); new CineFrame2(lesActeurs, lesFilms);}
+		if(bouton.equals("Menu")) { f.dispose(); new CineFrame1(lesActeurs, lesFilms);}
+		if(bouton.equals("Resoudre")) {
+			if(t1.getText().equals("") | t2.getText().equals("")){
+				new BoiteDialog(f, "Veuillez remplir tous les champs !!");
+			}
+			else{
+				Test.testGraphique(t1.getText(), t2.getText(), lesActeurs, lesFilms);
+			}
+		}
+		if(bouton.equals("JOUER")) {
+			if(t1.getText().equals("") | t2.getText().equals("")){
+				new BoiteDialog(f ,"Veuillez remplir tous les champs !!");
+			}
+			else{
+				Test.testGraphique(t1.getText(), t2.getText(), lesActeurs, lesFilms);
+			}
+		}
 		
 	}
 
