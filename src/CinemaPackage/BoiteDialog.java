543417c209b7aed5a333a816b8c6fb4675d0359c
Kolhtics <baptiste-cervantes@homtail.fr>
2012-03-28 14:53:21 +0200

    Ajout d'un boite de Dialog pour indiquer une erreur

---
+++ src/CinemaPackage/BoiteDialog.java
@@ -0,0 +1,79 @@
+package CinemaPackage;
+
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class BoiteDialog extends Dialog implements ActionListener, WindowListener{
+	private Frame f;
+	private String message;
+	
+	public BoiteDialog(Frame f, String message){
+		super(f, "Attention", true);
+		this.setLayout(new BorderLayout());
+		this.setSize(300,150);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(
+		        (screenSize.width-this.getWidth())/2,
+		        (screenSize.height-this.getHeight())/2
+		        );
+		
+		
+		Label l = new Label(message, Label.CENTER);
+		Button ok = new Button("Fermer");
+		this.add(l, BorderLayout.CENTER);
+		this.add(ok, BorderLayout.SOUTH);
+		
+		this.addWindowListener(this);
+		ok.addActionListener(this);
+		
+		this.setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		this.dispose();
+	}
+	
+	public void windowClosing(WindowEvent e){
+		this.dispose();
+	}
+
+	
+	// toutes ces methode ne servent a rien
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
