1a46e5219363859f09b3dc023027cd943e876c42
Kolhtics <baptiste-cervantes@homtail.fr>
2012-04-04 01:08:22 +0200

    ajout de verification si les acteur existe ou pas

--- src/Algorithme/AlgoJeu.java
+++ src/Algorithme/AlgoJeu.java
@@ -3,9 +3,7 @@ package Algorithme;
 import CinemaPackage.Acteur;
 import CinemaPackage.Film;
 import CinemaPackage.Repertoire;
-import Interface.BoiteDialog;
 import Interface.PanelJeu;
-import Interface.SolutionFrame;
 
 public class AlgoJeu {
 	private static int compteur=0;
@@ -14,9 +12,23 @@ public class AlgoJeu {
 		j.setLabelListe("Liste des films de "+act1);
 		Acteur a1= lesActeurs.rechercher(act1);
 		Acteur a2= lesActeurs.rechercher(act2);
+		if (a1 == null && a2 == null){
+			j.setLabelListe(act1+" n'existe pas");
+			j.setLabelCompteur(act2+" n'existe pas");
+		}
+		else if(a1 == null){
+			j.setLabelListe(act1+" n'existe pas");
+			j.setLabelCompteur("<- Umad???");
+		}
+		else if(a2 == null){
+			j.setLabelCompteur(act2+" n'existe pas");
+			j.setLabelListe("Umad??? ->");
+		}
+		else{
 		j.setListElement(a1);
 		j.setListReponse(a1.getId());
 		compteur=0;
+		}
 
 	}
 
@@ -48,12 +60,12 @@ public class AlgoJeu {
 		if (a != null){
 			j.setLabelListe("Liste des films de "+s);
 			j.setListElement(a);
+			compteur--;
 		}
 		else {
 			Film f= lesFilms.rechercher(s);
 			j.setLabelListe("Liste des acteurs de "+s);
 			j.setListElement(f);
-			compteur--;
 			j.setLabelCompteur("Nombre de coups : "+compteur);
 		}
 	}
