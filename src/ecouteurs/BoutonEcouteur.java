b52f85bbe5c439137860171572e7fca8a1e05638
Kolhtics <baptiste-cervantes@homtail.fr>
2012-05-09 23:43:08 +0200

    recherche et autre trucs

--- src/ecouteurs/BoutonEcouteur.java
+++ src/ecouteurs/BoutonEcouteur.java
@@ -2,10 +2,10 @@ package ecouteurs;
 
 import interfaceGraphique.*;
 
+import java.awt.List;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import algorithme.*;
 
 import cinemaPackage.*;
@@ -21,6 +21,7 @@ public class BoutonEcouteur implements ActionListener {
 	private PanelBot b;
 	private PanelJeu j;
 	private PanelRechercher r;
+	private List resultat;
 	
 	
 	public BoutonEcouteur(TextField t1, TextField t2, Repertoire<Acteur> lesActeurs, Repertoire<Film> lesFilms){
@@ -47,19 +48,18 @@ public class BoutonEcouteur implements ActionListener {
 
 	
 
-	public BoutonEcouteur(PanelRechercher r, TextField t1, Repertoire<Acteur> lesActeurs, Repertoire<Film> lesFilms) {
+	public BoutonEcouteur(PanelRechercher r, TextField t1, Repertoire<Acteur> lesActeurs, 
+			Repertoire<Film> lesFilms, List resultat) {
 		this.lesActeurs=lesActeurs;
 		this.lesFilms=lesFilms;
 		this.t1=t1;
 		this.r=r;
+		this.resultat=resultat;
 	}
 
 
 	public void actionPerformed(ActionEvent a) {
-		String bouton = a.getActionCommand();	
-		/*if(bouton.equals("Mode Jeu")) { f.setPanelJeu(); f.reconstruire(); }
-		if(bouton.equals("Mode Bot")) { f.setPanelBot(); f.reconstruire(); }
-		if(bouton.equals("Mode Recherche")) { f.setPanelImage(); f.reconstruire(); }*/
+		String bouton = a.getActionCommand();
 		if(bouton.equals("Resoudre")) {
 			if(t1.getText().equals("") | t2.getText().equals("")){
 				new BoiteDialog(f, "Veuillez remplir tous les champs !!");
@@ -70,9 +70,9 @@ public class BoutonEcouteur implements ActionListener {
 				b.setTexte(s);
 			}
 		}
-		
 		else if(bouton.equals("JOUER")) {
 			j.remiseZero();
+			j.setPanelInvisible();
 			if(t1.getText().equals("") | t2.getText().equals("")){
 				new BoiteDialog(f, "Veuillez remplir tous les champs !!");
 			}
@@ -80,8 +80,8 @@ public class BoutonEcouteur implements ActionListener {
 				AlgoJeu.jouer(j,t1.getText(), t2.getText(), lesActeurs, lesFilms);
 				j.validate();
 			}
+			j.setPanelVisible();
 		}
-		
 		else if(bouton.equals("Je suis bloque")){
 			if(t1.getText().equals("") | t2.getText().equals("")){
 				new BoiteDialog(f, "Veuillez remplir tous les champs !!");
@@ -90,7 +90,6 @@ public class BoutonEcouteur implements ActionListener {
 				new SolutionFrame(AlgoBot.jouer(t1.getText(), t2.getText(), lesActeurs, lesFilms));
 			}
 		}
-		
 		else if(bouton.equals("Ok")){
 			try{
 				AlgoJeu.suivant(j, j.getlistElement().getSelectedItem(), t2.getText(), lesActeurs, lesFilms);
@@ -100,7 +99,6 @@ public class BoutonEcouteur implements ActionListener {
 				new BoiteDialog(f , "Veuillez selectionner un objet dans la liste de gauche");
 			}
 		}
-		
 		else if(bouton.equals("Back")){ 
 			try{
 				AlgoJeu.back(j, lesActeurs, lesFilms); 
@@ -109,30 +107,32 @@ public class BoutonEcouteur implements ActionListener {
 			catch(ArrayIndexOutOfBoundsException e1){
 				new BoiteDialog(f , "Il n'y a plus rien a supprimer !");
 			}
-		
 		}
 		else if(bouton.equals("Afficher")){
 			try{
-				String s = AlgoRecherche.afficher_objet(t1.getText(), lesActeurs, lesFilms);
-				r.setTexte(s);
+				r.setTexte(AlgoRecherche.afficher_objet(t1.getText(), lesActeurs, lesFilms));
 			}
 			catch(NullPointerException e){
 				new BoiteDialog(f , "Ce film/acteur n'existe pas");
 			}
 		}
 		else if(bouton.equals("Rechercher un Acteur")){
-				String s = AlgoRecherche.recherche_Acteur(t1.getText(), lesActeurs, lesFilms, true);
-				r.setTexte(s);
+			if (t1.getText().length() > 2){
+				resultat.setVisible(false);
+				r.setListeA(AlgoRecherche.recherche_acteur(t1.getText(),lesActeurs));
+				resultat.setVisible(true);
+			}else {
+				new BoiteDialog(f, "Veuillez saisir 3 lettre ou plus");
+			}
 		}
 		else if(bouton.equals("Rechercher un Film")){
-
-				String s = AlgoRecherche.recherche_Acteur(t1.getText(), lesActeurs, lesFilms , false);
-				r.setTexte(s);
-
-		}
-		else if(bouton.equals("Test")){
-				String s = AlgoRecherche.recherche_test(t1.getText(), lesActeurs, lesFilms);
-				r.setTexte(s);
+			if (t1.getText().length() > 2){
+				resultat.setVisible(false);
+				r.setListeF(AlgoRecherche.recherche_film(t1.getText(),lesFilms));
+				resultat.setVisible(true);
+			}else {
+				new BoiteDialog(f, "Veuillez saisir 3 lettre ou plus");
+			}
 		}
 	}
 }
